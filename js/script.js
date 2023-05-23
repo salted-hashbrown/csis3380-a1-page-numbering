0b5d92e98ab501771188deb51c2896a05c142886
salted-hashbrown <105987464+salted-hashbrown@users.noreply.github.com>
2023-05-23 10:19:30 -0700

    pagination and load view
    added pagination, and load contact list view for pagination links

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 import { users } from './data.js';
 
-console.log(users);
+// console.log(users);
+
+const itemPerPage = 10;
 
 // fetching from api online, parse results, push to users array
 fetch('https://randomuser.me/api/?results=22')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        // console.log(data);
         // const test = data.results[0].name.first;
         // console.log(test);
         const fetchedUsers = [];
@@ -26,10 +28,60 @@ fetch('https://randomuser.me/api/?results=22')
             users.push(userObject);
         }
 
-        console.log('updated users');
-        console.log(users);
+        // console.log('updated users');
+        // console.log(users);
+
+        // total user count
+        const pageHeaderUserCountString = document.querySelector('.page-header h3');
+        // console.log(pageHeaderUserCountString);
+        pageHeaderUserCountString.innerHTML += `${users.length}`;
+
+        // pagination
+
+        const pageCount = Math.ceil(users.length / itemPerPage);
+        // console.log(`page count: ${pageCount}`);
+        const pagination = document.querySelector('.pagination');
+        // console.log(pagination.innerHTML);
+        for (let i = 0; i < pageCount; i++) {
+            pagination.innerHTML += `<li><a href="#")">` + (i + 1) + `</a></li>`;
+        }
+
+        // populate contact list view
+        function loadContactList(pageNumber) {
+            // console.log(`page number: ${pageNumber}`);
+            const contactList = document.querySelector('.contact-list');
+            // console.log(contactList.innerHTML);
+            contactList.innerHTML = '';
+            for (let i = 0; i < itemPerPage; i++) {
+                const j = (pageNumber - 1) * itemPerPage + i;
+                contactList.innerHTML += `<li class="contact-item cf">
+                    <div class="contact-details">
+                        <img class="avatar" src="${users[j].image}">
+                        <h3>${users[j].name}</h3>
+                        <span class="email">${users[j].name.replaceAll(' ', '.') + '@example.com'}</span>
+                    </div>
+                    <div class="joined-details">
+                        <span class="date">Joined ${users[j].joined}</span>
+                    </div>
+                    </li>`;
+                // console.log(contactList.innerHTML);
+            }
+        }
+
+        loadContactList(1);
+
+        const paginationLinks = document.querySelectorAll('.pagination li a');
+
+        paginationLinks.forEach((link, index) => {
+            link.addEventListener('click', (event) => {
+                // event.preventDefault();
+                const pageNumber = index + 1;
+                loadContactList(pageNumber);
+            });
+        });
 
     })
     .catch(error => {
         console.log('Error:', error);
     });
+
